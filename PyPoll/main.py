b1868208cc3916f282803ec50ee2e0d94fdedea6
Michelle Williams <michelleblanco@Michelles-MBP.attlocal.net>
2019-05-04 21:46:12 -0500

    This is my FINAL commit. I added more code to PyPoll main.py and updated psuedo code.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,25 +27,44 @@ with open(csvpath, newline='') as datafile:
     total_votes = len(raw_votes)
 
     #-----------------------------------------------------------------------
+    # Calculate total votes for each candidate
+
+    # Create dictionary (tally) using counter (c) to retrieve {'candidate': 'number of votes'}
     c = Counter(raw_votes)
     tally = dict(c)
+    
+    # Retrieve the key (candidate name) associated with the max number of votes
+    # Set it equal to a variable called 'winner'
     winner = max(tally, key=tally.get)
   
     #-----------------------------------------------------------------------
-    #Calculate total votes for each candidate
+    # Create lists 'candidates', 'votes_per_candidate', and 'percent_votes'
+
+    # Create list of candidates using the keys from the dictionary 'tally'
     candidates = [*tally.keys()]
+
+    # Create list of votes_per_candidate using the values from the dictionary 'tally'
     votes_per_candidate = [*tally.values()]
-    
+
+    # Create empty list called percent_votes
     percent_votes = []
+    # Loop through list 'votes_per_candidate'
     for x in votes_per_candidate:
-        percent = '{:.3%}'.format(x/total_votes)
+        # find percent by taking votes per candidate/total_votes. Format as percent and keep 3 decimal places.
+        percent = '{:.3%}'.format(x/total_votes) 
+        # Append percent found to the list 'percent_votes'
         percent_votes.append(percent)
+
+#-----------------------------------------------------------------------
+# Print Election Results to terminal
 
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
+# Loop through all indexes given the length of candidates list
 for i in range(len(candidates)):
+        # Print index i of 'candidates', 'percent votes', and 'votes_per_candidate'
         print(f"{candidates[i]}: {percent_votes[i]} ({votes_per_candidate[i]})")
 print("-------------------------")
 print(f"Winner: {winner}")
@@ -55,15 +74,17 @@ print("-------------------------")
 # Open new file(f) 'output.txt' to write output to
 f = open("output.txt",'w')
 
+#-------------------------------------------------- 
 # Write output(Financial Analysis) to file(f)
 f.writelines(f'''Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------\n''')
-
+# Loop through all indexes given the length of candidates list
 for i in range(len(candidates)):
+        # Write lines to file using index i of 'candidates', 'percent votes', and 'votes_per_candidate'
         f.writelines(f"{candidates[i]}: {percent_votes[i]} ({votes_per_candidate[i]})\n")
-
+# Continue writing lines to file (f)
 f.writelines(f'''-------------------------
 Winner: {winner}
 -------------------------''')
